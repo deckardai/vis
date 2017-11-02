7e859aeafde28d6fd65d9a25add6b23e910e9bdb
Pascal Gula <pascal.gula@gmail.com>
2017-11-02 15:59:51 +0000

    further cluster updates and docs

--- index-timeline-deckard.js
+++ index-timeline-deckard.js
@@ -8,6 +8,21 @@ exports.Timeline = require('./lib/timeline/Timeline');
   - getVisibleItems returns an array composed of all visible items
   - 
 
+  Note:
+  * Core:
+    - setWindow is modifying this.range through setRange -> investigate there
+    - zoomIn/zoomOut call setWindow so nothing to do there
+    - in redraw, the components are being iteratively redraw at the end of the function,
+      therefore, we should look there...
+  * Timeline:
+    - the items are stored in itemsData and itemSet is populated with the same
+    - there is a Configurator that does something...
+    - 
+  * ItemSet:
+    - from getVisibleItems, we lean that each group has an array of visibleItems
+    => our ClusterItem will therefore land here.
+    - back @ redraw() method!
+
   Build: browserify index-timeline-deckard.js -t [ babelify --presets [es2015] ] -o dist/vis-deckard.js -s vis
   Minify: uglifyjs dist/vis-deckard.js -o dist/vis-deckard.min.js
 
